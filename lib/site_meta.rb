9669c25ba7d2075e338dd5425961252eb7b1fe91
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2009-01-04 12:41:31 +0100

    Implemented initial version of helpers

--- lib/site_meta.rb
+++ lib/site_meta.rb
@@ -1,6 +1,71 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
 module SiteMeta
   VERSION = '0.1'
+  
+  def head_title(default_title, separator=" - ")
+    title = [@head_title, default_title].flatten.compact.join(separator)
+    title_tag(title)
+  end
+  
+  def meta_description(default_description=nil)
+    description = @meta_description || default_description
+    if description
+      meta_tag(:description, description)
+    end
+  end
+  
+  def split_keywords(keywords)
+    return [] unless keywords
+    keywords.split(",")
+  end
+  
+  def meta_keywords(default_keywords=nil)
+    merge_mode = @meta_keywords_merge_mode || :merge
+    meta_keywords = split_keywords(@meta_keywords)
+    keywords = if merge_mode == :merge
+      default_keywords = split_keywords(default_keywords)
+      (meta_keywords + default_keywords).uniq
+    else
+      meta_keywords
+    end
+    
+    unless keywords.empty?
+      meta_tag(:keywords, keywords.join(","))
+    end
+  end
+  
+  def meta_tag(name, content)
+    "<meta name=\"#{name}\" content=\"#{content}\" />"
+  end
+  
+  def title_tag(content)
+    "<title>#{content}</title>"
+  end
+  
+  def page_title
+    @page_title
+  end
+
+  def set_head_title(title)
+    @head_title = title
+  end
+  
+  def set_head_and_page_title(title)
+    title = [title].flatten.compact
+    set_head_title title
+    set_page_title title.first
+  end
+  
+  def set_page_title(title)
+    @page_title = title
+  end
+  
+  def set_meta_description(description)
+    @meta_description = description
+  end
+  
+  def set_meta_keywords(keywords, merge_mode=:merge)
+    raise(ArgumentError, "Allowed merge modes are only :replace, :merge") unless [:replace,:merge].include?(merge_mode) 
+    @meta_keywords = keywords
+    @meta_keywords_merge_mode = merge_mode
+  end
 end
--- spec/site_meta_spec.rb
+++ spec/site_meta_spec.rb
@@ -1,11 +1,126 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
-  
-  it "find this spec in spec directory" do
-    violated "Be sure to write your specs"
+class SiteMetaInstance
+  include SiteMeta
+end
+
+class NilClass
+  def blank?
+    true
+  end
+end
+
+describe SiteMeta do
+  before(:each) do
+    @helper = SiteMetaInstance.new
   end
   
+  it "should use given name and content for meta tag" do
+    @helper.meta_tag(:description, "Default").should == '<meta name="description" content="Default" />'
+  end
+  
+  it "should use provided content for title tag" do
+    @helper.title_tag("Content").should == "<title>Content</title>"
+  end
+  
+  describe "when providing description" do
+    it "should accept empty default description" do
+      @helper.meta_description
+    end
+    
+    it "should not insert empty description meta tag" do
+      @helper.meta_description.should be_blank
+    end
+    
+    it "should use default description" do
+      @helper.meta_description("Default").should == @helper.meta_tag(:description, "Default")
+    end
+    
+    it "should use provided description" do
+      @helper.set_meta_description("Non-default")
+      @helper.meta_description("Default").should == @helper.meta_tag(:description, "Non-default")
+    end
+  end
+
+  describe "when providing keywords" do
+    it "should accept empty default keywords" do
+      @helper.meta_keywords
+    end
+    
+    it "should not insert empty keywords meta tag" do
+      @helper.meta_keywords.should be_blank
+    end
+    
+    it "should use default keywords" do
+      @helper.meta_keywords("default,keywords").should == @helper.meta_tag(:keywords, "default,keywords")
+    end
+    
+    it "should merge provided keywords by default" do
+      @helper.set_meta_keywords("non,default")
+      @helper.meta_keywords("default,keywords").should == @helper.meta_tag(:keywords, "non,default,keywords")
+    end
+
+    it "should merge provided keywords" do
+      @helper.set_meta_keywords("non,default", :merge)
+      @helper.meta_keywords("default,keywords").should == @helper.meta_tag(:keywords, "non,default,keywords")
+    end
+
+    it "should replace provided keywords" do
+      @helper.set_meta_keywords("non,default", :replace)
+      @helper.meta_keywords("default,keywords").should == @helper.meta_tag(:keywords, "non,default")
+    end
+
+    it "should reject unknown merge mode" do
+      lambda {
+        @helper.set_meta_keywords("non,default", :overwrite)
+      }.should raise_error(ArgumentError, "Allowed merge modes are only :replace, :merge")
+    end
+  end
+  
+  describe "when providing head title" do
+    it "should provide default title" do
+      @helper.head_title("Site name").should == @helper.title_tag("Site name")
+    end
+    
+    it "should accept string as title" do
+      @helper.set_head_title("Page title")
+      @helper.head_title("Site name").should == @helper.title_tag("Page title - Site name")
+    end
+    
+    it "should accept array as title" do
+      @helper.set_head_title(["Page title"])
+      @helper.head_title("Site name").should == @helper.title_tag("Page title - Site name")
+    end
+    
+    it "should accept multiple entries as title" do
+      @helper.set_head_title(["Item title", "View title"])
+      @helper.head_title("Site name").should == @helper.title_tag("Item title - View title - Site name")
+    end
+
+    it "should use custom separator" do
+      @helper.set_head_title("Page title")
+      @helper.head_title("Site name", " &raquo; ").should == @helper.title_tag("Page title &raquo; Site name")
+    end
+  end
+  
+  describe "when providing page title" do
+    it "should use given title" do
+      @helper.set_page_title("Page title")
+      @helper.page_title.should == "Page title"
+    end
+  end
+  
+  describe "when providing page and head title" do
+    it "should accept string title" do
+      @helper.set_head_and_page_title("Page title")
+      @helper.page_title.should == "Page title"
+      @helper.head_title("Site name").should == @helper.title_tag("Page title - Site name")
+    end
+    
+    it "should accept array title" do
+      @helper.set_head_and_page_title(["Item title", "View title"])
+      @helper.page_title.should == "Item title"
+      @helper.head_title("Site name").should == @helper.title_tag("Item title - View title - Site name")
+    end
+  end
 end
